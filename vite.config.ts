4d82c6f31c368368592a466b27f3c3d272be5f4f
Drikus Roor <drikusroor@gmail.com>
2024-12-26 21:37:51 +0100

    fix: Update Vite configuration to include test files for Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +11,7 @@ export default defineConfig({
     },
   },
   test: {
+    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
     globals: true,
     environment: 'jsdom',    // so we can test React components, DOM APIs, etc.
     // coverage, mocks, etc. can go here if desired
